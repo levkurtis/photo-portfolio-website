c977e5a95ffc3d6494a3d6fbbbd6180a4670b2f4
Levent Kurtis <leventkurtis@Levents-MBP.lan>
2020-12-10 11:23:10 +0100

    img/a unselectable, modal cards, esc/arrow fncts

--- gallery.js
+++ gallery.js
@@ -8,31 +8,42 @@ function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
 }
+
+document.onkeydown = function (event) {
+  var key = event.key || event.keyCode;
+
+  if (key === "Escape" || key === "Esc" || key === 27) {
+    closeModal();
+  } else if (key === "ArrowLeft" || key === 37) {
+    plusSlides(-1);
+  } else if (key === "ArrowRight" || key === 39) {
+    plusSlides(1);
+  }
+};
