bab150758343b64342c393e472b3cc2816f333bb
Levent Kurtis <levkurtis@gmail.com>
2020-12-22 18:54:18 +0100

    Lightbox2

--- js/gallery.js
+++ js/gallery.js
@@ -1,49 +1,49 @@
-// Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
+// // Open the Modal
+// function openModal() {
+//   document.getElementById("myModal").style.display = "block";
+// }
 
-// Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
+// // Close the Modal
+// function closeModal() {
+//   document.getElementById("myModal").style.display = "none";
+// }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+// }
 
-document.onkeydown = function (event) {
-  var key = event.key || event.keyCode;
+// document.onkeydown = function (event) {
+//   var key = event.key || event.keyCode;
 
-  if (key === "Escape" || key === "Esc" || key === 27) {
-    closeModal();
-  } else if (key === "ArrowLeft" || key === 37) {
-    plusSlides(-1);
-  } else if (key === "ArrowRight" || key === 39) {
-    plusSlides(1);
-  }
-};
+//   if (key === "Escape" || key === "Esc" || key === 27) {
+//     closeModal();
+//   } else if (key === "ArrowLeft" || key === 37) {
+//     plusSlides(-1);
+//   } else if (key === "ArrowRight" || key === 39) {
+//     plusSlides(1);
+//   }
+// };
